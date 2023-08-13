10292d9f89795e5eb08f28a42a8a774ff1797aad
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-08-13 23:20:30 +0600

    practice project

---
+++ practice.sql
@@ -0,0 +1,162 @@
+-- Active: 1691480676960@@127.0.0.1@5432@practice_task1
+-- Task 1: SELECT and WHERE Clause Create a table named "employees" with columns (emp_id, emp_name, department, salary) and insert the following data:
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department VARCHAR(50),
+    salary DECIMAL(10, 2)
+);
+
+INSERT INTO employees (emp_id, emp_name, department, salary)
+VALUES
+    (1, 'John Doe', 'HR', 50000.00),
+    (2, 'Jane Smith', 'IT', 60000.00),
+    (3, 'Michael Johnson', 'Finance', 55000.00),
+    (4, 'Emily Brown', 'HR', 52000.00);
+select * from employees;
+
+--- Write an SQL query to retrieve the names and salaries of employees who work in the "HR" department.
+
+select emp_name, salary from employees
+WHERE department = 'HR';
+
+
+-- Task 2: Aggregation and HAVING Clause Create a table named "orders" with columns (order_id, customer_id, total_amount) and insert the following data:
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    total_amount DECIMAL(10, 2)
+);
+
+INSERT INTO orders (order_id, customer_id, total_amount)
+VALUES
+    (101, 1, 200.00),
+    (102, 2, 300.00),
+    (103, 1, 150.00),
+    (104, 3, 400.00),
+    (105, 2, 250.00);
+
+    SELECT * FROM orders;
+
+-- Write an SQL query to find the customer IDs and the average total amount of their orders. Display only those customers whose average total amount is greater than or equal to 250.
+SELECT customer_id, avg(total_amount) as average_total_amount FROM orders
+GROUP BY customer_id;
+
+SELECT customer_id, avg(total_amount) as average_total_amount FROM orders
+GROUP BY customer_id HAVING avg(total_amount) >= 250;
+
+-- Task 3: JOIN and GROUP BY Create two tables named "students" and "courses" with columns as follows:
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(50),
+    age INT,
+    gender VARCHAR(10)
+);
+
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(50),
+    credits INT
+);
+
+INSERT INTO students (student_id, student_name, age, gender)
+VALUES
+    (1, 'Alice', 22, 'Female'),
+    (2, 'Bob', 21, 'Male'),
+    (3, 'Charlie', 23, 'Male');
+
+INSERT INTO courses (course_id, course_name, credits)
+VALUES
+    (101, 'Mathematics', 3),
+    (102, 'History', 4),
+    (103, 'Physics', 3);
+
+-- Enrollment table with student-course relationships
+CREATE TABLE enrollment (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT
+);
+
+INSERT INTO enrollment (enrollment_id, student_id, course_id)
+VALUES
+    (1, 1, 101),
+    (2, 1, 102),
+    (3, 2, 103),
+    (4, 3, 101);
+
+-- Write an SQL query to retrieve the student name, course name, and credits for all enrolled courses.
+SELECT s.student_name, cr.course_name, cr.credits
+FROM students s
+JOIN enrollment e on e.student_id = s.student_id
+JOIN courses cr ON cr.course_id = e.course_id;
+
+CREATE DATABASE practice_task1;
+
+-- Task 4: Multiple Joins and Aggregation Create three tables named "employees," "departments," and "salaries" with columns as follows:
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department_id INT
+);
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(50)
+);
+CREATE TABLE salaries (
+    emp_id INT,
+    salary DECIMAL(10, 2)
+);
+INSERT INTO employees (emp_id, emp_name, department_id)
+VALUES
+    (1, 'John Doe', 1),
+    (2, 'Jane Smith', 2),
+    (3, 'Michael Johnson', 1),
+    (4, 'Emily Brown', 3);
+INSERT INTO departments (department_id, department_name)
+VALUES
+    (1, 'HR'),
+    (2, 'IT'),
+    (3, 'Finance');
+INSERT INTO salaries (emp_id, salary)
+VALUES
+    (1, 50000.00),
+    (2, 60000.00),
+    (3, 55000.00),
+    (4, 52000.00);
+-- Write an SQL query to retrieve the department name and the average salary of employees working in each department. Sort the results by the average salary in descending order.
+
+SELECT d.department_name, avg(s.salary)
+FROM employees e
+JOIN salaries s on s.emp_id = e.emp_id
+JOIN departments d on e.department_id = d.department_id
+GROUP BY d.department_name;
+
+SELECT d.department_name, avg(s.salary)
+FROM employees e
+JOIN salaries s on s.emp_id = e.emp_id
+JOIN departments d on e.department_id = d.department_id
+GROUP BY d.department_name
+ORDER BY avg(salary) DESC;
+
+-- Task 5: Aggregation and Grouping Create a table named "orders" with columns (order_id, customer_id, order_date, total_amount) and insert the following data:
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2)
+);
+INSERT INTO orders (order_id, customer_id, order_date, total_amount)
+VALUES
+    (101, 1, '2023-01-05', 200.00),
+    (102, 2, '2023-01-06', 300.00),
+    (103, 1, '2023-02-10', 150.00),
+    (104, 3, '2023-02-15', 400.00),
+    (105, 2, '2023-03-20', 250.00);
+-- Write an SQL query to find the total sales amount for each month, along with the number of orders in that month.
+SELECT * FROM orders;
+
+SELECT to_char(order_date, 'Month'), SUM(total_amount), count(*)
+FROM orders
+GROUP BY to_char(order_date, 'Month');
